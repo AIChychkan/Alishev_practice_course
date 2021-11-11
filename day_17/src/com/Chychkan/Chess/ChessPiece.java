1483f39b48dc7dcc812994d14ed4c593d401fbb4
AIChychkan <a.i.chychkan@gmail.com>
2021-11-11 13:08:56 +0200

    Added day_17 - day_19

---
+++ day_17/src/com/Chychkan/Chess/ChessPiece.java
@@ -0,0 +1,5 @@
+package com.Chychkan.Chess; /** 
+   * Created by A.I.Chychkan on 08/11/2021
+   */
+ public enum ChessPiece {
+}
---
+++ day_19/Task_2_taxi/src/com/Chychkan/taxi/Point.java
@@ -0,0 +1,5 @@
+package com.Chychkan.taxi; /** 
+   * Created by A.I.Chychkan on 11/11/2021
+   */
+ public class Point {
+}
